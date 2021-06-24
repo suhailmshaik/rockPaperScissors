f2a88f813bdca08db1589a43024e4f41b8e5e6d4
suhail <shaikmsuhail02@gmail.com>
2021-06-24 20:41:25 +0530

    updated: it will show special message for people who enter wrong spellings

--- script.js
+++ script.js
@@ -14,19 +14,21 @@ function computerPlay() {
 computerSelection = computerPlay();
 playerSelection = prompt("pick one: rock,paper or scissors").toLowerCase();
 
-function playRound (playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) {
   if (playerSelection == "rock" && computerSelection == "paper") {
-    return "you lose! " + computerSelection + " beats " + playerSelection; 
-  } else if ( playerSelection == "paper" && computerSelection == "scissors") {
-    return "you lose! " + computerSelection + " beats " + playerSelection; 
+    return "you lose! " + computerSelection + " beats " + playerSelection;
+  } else if (playerSelection == "paper" && computerSelection == "scissors") {
+    return "you lose! " + computerSelection + " beats " + playerSelection;
   } else if (playerSelection == "scissors" && computerSelection == "rock") {
-    return "you lose! " + computerSelection + " beats " + playerSelection; 
+    return "you lose! " + computerSelection + " beats " + playerSelection;
   } else if (playerSelection == computerSelection) {
     return "it's a tie!";
+  } else if (playerSelection != computerSelection) {
+    return "please enter the correct spelling for rock, paper, scissors and stop fucking around u bitch!";
   } else {
     return "you won " + playerSelection + " beats " + computerSelection;
   }
-} 
+}
 
 console.log(playRound(playerSelection, computerSelection));
-// console.log(playerSelection, computerSelection);
+console.log(playerSelection, computerSelection);
