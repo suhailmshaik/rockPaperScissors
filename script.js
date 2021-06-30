f88ec3d88c7e9f275c7a3b355106d681eba7b331
suhail <shaikmsuhail02@gmail.com>
2021-06-30 12:24:32 +0530

    added comments for better understanding and removed previous commented code

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// computerPlay() picks either rock, paper or scissors randomly
+
 function computerPlay() {
   let computer = Math.floor(Math.random() * 3) + 1;
   if (computer == 1) {
@@ -9,25 +11,23 @@ function computerPlay() {
   }
 }
 
+// variables "c" and "p" store the scores for the game
+// c for computer and p for player
+// c increases by 1 if computer wins in a round and p increases by 1 if player wins a round
+
 let c = 0;
 let p = 0;
 
-// console.log(computerSelection);
-let playerSelection;
-// playerSelection = prompt("pick one: rock,paper or scissors").toLowerCase();
-// computerSelection;
+// playRound() asks the user for an input through prompt() and assigns the input as playerSelection
+// computerPlay() value is assigned as computerSelection
+// then it evaluates the following logic and gives out the result(round's winner) through alert
+// rock > scissors
+// paper > rock
+// scissors > paper
 
 function playRound() {
   computerSelection = computerPlay();
   playerSelection = prompt("pick one: rock,paper or scissors").toLowerCase();
-
-  // console.log(playerSelection, computerSelection);
-
-  // if (playerSelection != computerSelection) {
-  //   alert(
-  //     "please enter the correct spelling for rock, paper, scissors and stop fucking around u bitch!"
-  //   );
-  // } else
 
   if (playerSelection == "rock" && computerSelection == "paper") {
     c = c + 1;
@@ -43,12 +43,7 @@ function playRound() {
     return c;
   } else if (playerSelection == computerSelection) {
     alert("it's a tie!");
-  }
-
-  // else if (playerSelection != computerSelection) {
-  //   alert(
-  //     "please enter the correct spelling for rock, paper, scissors and stop fucking around u bitch!"
-  else {
+  } else {
     p = p + 1;
     alert("you won " + playerSelection + " beats " + computerSelection);
     //   );
@@ -56,11 +51,10 @@ function playRound() {
   }
 }
 
-// playRound(
-//   (playerSelection = prompt("pick one: rock,paper or scissors").toLowerCase()),
-//   computerSelection
-// );
-// console.log(playerSelection, computerSelection);
+// game() runs playRound() for 5 times and calculates the values of "c" and "p"
+// if c > p = computer won
+// else if p > c = player won
+// and gives out an alert on who won the game
 
 function game() {
   for (i = 0; i < 5; i++) {
